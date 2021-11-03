751c3efc40551d5b1899262d261ecf1451f30f79
averagephotographer <christopher.tullier98@gmail.com>
2021-11-03 14:26:49 -0500

    making minimax function

---
+++ minimax.py
@@ -0,0 +1,18 @@
+def minimax(position, depth, alpha, beta, maximizingPlayer):
+    
+    if depth == 0 or gameIsOver:
+        return score # static evaluation of position
+
+    if maximizingPlayer:
+        maxEval = float("-inf")
+        for child in position:
+            tempEval = minimax(child, depth - 1, False)
+            maxEval = max(maxEval, eval)
+        return maxEval
+        
+    else:
+        minEval = float("inf")
+        for child in position:
+            tempEval = minimax(child, depth - 1, True)
+            minEval = min(minEval, eval)
+        return minEval
